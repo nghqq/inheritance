729a3c03931cf8d5072f2241ea429e6761eac79c
- <-@DESKTOP-5LUH4K3>
2023-09-12 14:12:38 +0300

    DONE

---
+++ Files/main.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include<fstream>
+
+void main() 
+{
+	setlocale(LC_ALL, "");
+
+	std::ofstream fout;			// Создаем поток
+	fout.open("File.txt");		// Открывает поток
+	fout << "Hello Files";		//Пишем в поток
+	fout.close();				//Когда поток не нужен его нужно закрыть 
+
+	system("notepad File.txt");  // 'system' позволяет вызвать любую команду из командной строки
+}
